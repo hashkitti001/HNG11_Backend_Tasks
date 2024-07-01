aaf518310d0266a31d6b28d77340955c085c43b7
hashkitti001 <iJeddy5.0@gmail.com>
2024-07-01 01:41:17 +0100

    add server.js

---
+++ stageone/server.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+const { default: axios } = require('axios');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 9999;
+
+app.use(express.json());
+
+app.get('/api/hello', async (req, res) => {
+  try {
+    const { visitor_name } = req.query;
+    const client_ip = req.ip
+    if(!visitor_name){
+        return res.status(400).json({
+            "message":"You must include visitor_name in your query"
+        })
+    }
+    
+    const datafromIP = await axios.get(
+            `http://api.ipapi.com/api/${client_ip}?access_key=${process.env.IPAPI_KEY}`
+    );
+
+    const { city, country_name } = datafromIP?.data;
+
+    const weatherData = await axios.get(
+            `https://api.weatherapi.com/v1/current.json?key=${process.env.WEATHERAPI_KEY}&q=${country_name}`
+    );
+    const { temp_c } = weatherData.data.current;
+
+    const result = {
+      client_ip,
+      location: city,
+      greeting: `Hello ${visitor_name}! the temperature is ${temp_c} degree Celsius in ${city}`
+    };
+    return res.status(200).json(result);
+  } catch (e) {
+    console.log(e.message);
+    return res.status(500).json(e.message);
+  }
+});
+app.listen(PORT, () => {
+  console.log('Listening on port', PORT);
+});
