95934044d1d100b68864112bfa5c24393b037f24
hashkitti001 <iJeddy5.0@gmail.com>
2024-07-02 12:58:23 +0100

    fix: format client IP address

--- stageone/server.js
+++ stageone/server.js
@@ -3,13 +3,22 @@ const { default: axios } = require('axios');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 9999;
-
+const requestIp = require("request-ip")
+app.use(requestIp.mw())
 app.use(express.json());
 
 app.get('/api/hello', async (req, res) => {
   try {
     const { visitor_name } = req.query;
-    const client_ip = req.ip
+    const clientIp = req.clientIp
+   
+    if (clientIp === '::1' || clientIp === '::ffff:127.0.0.1') {
+        clientIp = '127.0.0.1';
+    }
+    if (clientIp.startsWith('::ffff:')) {
+        clientIp = clientIp.replace('::ffff:', '');
+    }
+
     if(!visitor_name){
         return res.status(400).json({
             "message":"You must include visitor_name in your query"
@@ -19,7 +28,7 @@ app.get('/api/hello', async (req, res) => {
     const datafromIP = await axios.get(
             `http://api.ipapi.com/api/${client_ip}?access_key=${process.env.IPAPI_KEY}`
     );
-
+    
     const { city, country_name } = datafromIP?.data;
 
     const weatherData = await axios.get(
@@ -34,7 +43,7 @@ app.get('/api/hello', async (req, res) => {
     };
     return res.status(200).json(result);
   } catch (e) {
-    console.log(e.message);
+    console.trace(e.message);
     return res.status(500).json(e.message);
   }
 });
