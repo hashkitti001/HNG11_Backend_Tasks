bdde23c44bdc94d88c3944555f8c557263ec0610
hashkitti001 <iJeddy5.0@gmail.com>
2024-07-06 23:13:44 +0100

    fix: security patch to hash user passwords on register endpoint

--- stagetwo/controllers/userController.js
+++ stagetwo/controllers/userController.js
@@ -1,12 +1,17 @@
 const User = require("../models/User")
 const uuid = require("uuid")
+const bcrypt = require("bcryptjs")
 
 const registerUser = async (req, res) => {
     const { firstName, lastName, email, password, phone } = req.body
     try {
+        const salt = await bcrypt.genSalt()
+        const hashedPassword = bcrypt.hash(password, salt)
         const newUser = await User.create({
-            userId:String(uuid.v4()), 
-            firstName, lastName, email, password, phone
+            userId: String(uuid.v4()),
+            firstName, lastName, email,
+            password: hashedPassword,
+            phone
         })
         res.status(201).json({
             status: "success",
@@ -21,19 +26,19 @@ const registerUser = async (req, res) => {
                 }
             }
         })
-    } catch(e){
-        if(e.errors){
+    } catch (e) {
+        if (e.errors) {
             const errors = e.errors.map(err => ({
                 field: err.path,
                 message: err.message
             }))
-            res.status(422).json({errors})
+            res.status(422).json({ errors })
         } else {
             console.error("Error registering user", error)
-            res.status(500).json({message: "Internal server error"})
+            res.status(500).json({ message: "Internal server error" })
         }
     }
-    
+
 }
 
 module.exports = {
