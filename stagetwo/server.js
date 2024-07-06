db7d029b5c591a7867584af5f885fe52246d4f3d
hashkitti001 <iJeddy5.0@gmail.com>
2024-07-07 00:36:45 +0100

    fragment of adding private routes

--- stagetwo/server.js
+++ stagetwo/server.js
@@ -4,10 +4,14 @@ const morgan = require("morgan")
 const PORT = process.env.PORT || 9999
 const { dbConn } = require("./utils/dbConnect")
 const indexRouter = require("./routes/public")
+const privateRouter = require("./routes/private")
 app.use(morgan("dev"))
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+
 app.use(indexRouter)
+app.use(privateRouter)
+
 dbConn()
 app.listen(PORT, () => {
     console.info("Stage 2 API running on port", PORT)
